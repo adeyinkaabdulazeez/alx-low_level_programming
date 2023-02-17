db6a242f1e0216c710cd144f3c68381687770d3d
adeyinkaabdulazeez <abdulazeezadeyinka0804@gmail.com>
2023-02-17 08:13:38 -0800

    ddh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line
- *  Return: (0) (success)
+ * main - Entry point
  *
-int main(void){
-    puts("\'Programming is like building a multilingual puzzle");
-    return 0;
-    }
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
